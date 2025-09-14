0a478cad4964d712e442400fa97e46b318ef3d91
nicoalesi <nicoalesi.cs@gmail.com>
2025-09-14 14:16:02 +0200

    all

---
+++ build/tests/cpp/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/nico/temp/try/tests/cpp
+# Build directory: /home/nico/temp/try/build/tests/cpp
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/home/nico/temp/try/build/tests/cpp/unit_tests[1]_include.cmake")
